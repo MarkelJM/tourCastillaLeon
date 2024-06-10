e00ed36be423e81a25cee884f2715ab001da25a7
Markel Juaristi <markeljmdeveloper@gmail.com>
2024-06-10 12:24:34 +0200

    installed Firebase

--- wowCastillaLeon/wowCastillaLeon/wowCastillaLeonApp.swift
+++ wowCastillaLeon/wowCastillaLeon/wowCastillaLeonApp.swift
@@ -7,9 +7,29 @@
 
 import SwiftUI
 
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+    FirebaseApp.configure()
+
+    return true
+
+  }
+
+}
+
+
+
 @main
 struct wowCastillaLeonApp: App {
     let persistenceController = PersistenceController.shared
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
