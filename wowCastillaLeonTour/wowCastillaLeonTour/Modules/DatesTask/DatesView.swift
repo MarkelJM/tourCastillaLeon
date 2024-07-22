dae367021abbb7fc798f775aa1fa23061d0fcd2f
Markel Juaristi <markeljmdeveloper@gmail.com>
2024-07-22 10:01:35 +0200

    created every task view empty

---
+++ wowCastillaLeonTour/wowCastillaLeonTour/Modules/DatesTask/DatesView.swift
@@ -0,0 +1,18 @@
+//
+//  DatesView.swift
+//  wowCastillaLeonTour
+//
+//  Created by Markel Juaristi on 22/7/24.
+//
+
+import SwiftUI
+
+struct DatesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DatesView()
+}
---
+++ wowCastillaLeonTour/wowCastillaLeonTour/Modules/QuestionAnswerTask/QuestionAnswerView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionAnswerView.swift
+//  wowCastillaLeonTour
+//
+//  Created by Markel Juaristi on 22/7/24.
+//
+
+import SwiftUI
+
+struct QuestionAnswerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionAnswerView()
+}
