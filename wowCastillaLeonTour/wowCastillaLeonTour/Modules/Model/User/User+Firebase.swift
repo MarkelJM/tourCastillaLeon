0141ae7cb424b59823f2acf1a60b4a4957091a31
Markel Juaristi <markeljmdeveloper@gmail.com>
2024-07-24 11:30:08 +0200

    adapted User+Firebase model to tasks id list

--- wowCastillaLeonTour/wowCastillaLeonTour/Modules/Model/User/User+Firebase.swift
+++ wowCastillaLeonTour/wowCastillaLeonTour/Modules/Model/User/User+Firebase.swift
@@ -20,7 +20,10 @@ extension User {
               let provinceString = firestoreData["province"] as? String,
               let province = Province(rawValue: provinceString),
               let avatarString = firestoreData["avatar"] as? String,
-              let avatar = Avatar(rawValue: avatarString) else {
+              let avatar = Avatar(rawValue: avatarString),
+              let taskIDs = firestoreData["taskIDs"] as? [String],
+              let coinTaskIDs = firestoreData["coinTaskIDs"] as? [String],
+              let gadgetTaskIDs = firestoreData["gadgetTaskIDs"] as? [String] else {
             return nil
         }
         
@@ -33,6 +36,9 @@ extension User {
         self.city = city
         self.province = province
         self.avatar = avatar
+        self.taskIDs = taskIDs
+        self.coinTaskIDs = coinTaskIDs
+        self.gadgetTaskIDs = gadgetTaskIDs
     }
     
     func toFirestoreData() -> [String: Any] {
@@ -45,7 +51,10 @@ extension User {
             "postalCode": postalCode,
             "city": city,
             "province": province.rawValue,
-            "avatar": avatar.rawValue
+            "avatar": avatar.rawValue,
+            "taskIDs": taskIDs,
+            "coinTaskIDs": coinTaskIDs,
+            "gadgetTaskIDs": gadgetTaskIDs
         ]
     }
 }
