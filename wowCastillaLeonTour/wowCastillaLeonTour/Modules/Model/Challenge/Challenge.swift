a5f21925177942bbdae26d86a33ea05446c33485
Markel Juaristi <markeljmdeveloper@gmail.com>
2024-08-24 18:08:26 +0200

    added spot and challenges model and extensions

---
+++ wowCastillaLeonTour/wowCastillaLeonTour/Modules/Model/Challenge/Challenge.swift
@@ -0,0 +1,21 @@
+//
+//  Challenge.swift
+//  wowCastillaLeonTour
+//
+//  Created by Markel Juaristi on 24/8/24.
+//
+
+import Foundation
+
+struct Challenge: Identifiable, Codable {
+    var id: String // Este campo puede ser el challengeID
+    var challengeName: String
+    var challengeTaskIDs: [String]
+    var challengeTitle: String
+    var correctAnswerMessage: String
+    var incorrectMessage: String
+    var image: String
+    var isBegan: Bool
+    var province: String
+    var taskAmount: Int
+}
